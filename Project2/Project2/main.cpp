f96414b81d9749c348651f899ba5538c9a06f58e
Eric Navarro de Santiago <lakaens12@gmail.com>
2017-03-03 10:39:40 +0100

    Quadrat en SDL

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "SDL\SDL.h"
+
+int main(int argc, char* args[]) {
+	int SDL_RenderDrawRect(SDL_Renderer*   renderer,
+		const SDL_Rect* rect);
+
+
+
+	system("pause");
+	return 0;
+}
