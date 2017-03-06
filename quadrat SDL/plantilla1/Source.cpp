6cadd919db2ae02badfed54f8abaebf0758233da
Eric Navarro de Santiago <lakaens12@gmail.com>
2017-03-06 10:14:12 +0100

    Quadrat sdl

--- quadrat SDL/plantilla1/Source.cpp
+++ quadrat SDL/plantilla1/Source.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* args[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	g_pWindow = SDL_CreateWindow("Chapter 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
 	if (g_pWindow != 0) {
-		g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, SDL_RENDERER_PRESENTVSYNC);
 	}
 	else {
 		return 1;
@@ -29,39 +29,66 @@ int main(int argc, char* args[]) {
 
 	SDL_Event event;
 	bool running = true;
-	int x;
+	bool up = false, down = false, right = false, left=false, space=false;
 	while (running) {
 		if (SDL_PollEvent(&event)) {
-			if (event.type = SDL_KEYDOWN) {
+			if (event.type == SDL_KEYUP) {
+				switch (event.key.keysym.sym) {
+				case SDLK_UP:
+					up = false;
+					break;
+				case SDLK_DOWN:
+					down = false;
+					break;
+				case SDLK_RIGHT:
+					right = false;
+					break;
+				case SDLK_LEFT:
+					left = false;
+					break;
+				case SDLK_SPACE:
+					space = true;
+				default:
+					break;
+				}
+			}
+			if (event.type == SDL_KEYDOWN) {
 				switch (event.key.keysym.sym) {
 				case SDLK_ESCAPE:
 					running = false;
 					break;
 				case SDLK_UP:
-					if (rectangle.y != 0)rectangle.y -= 5;
+					up = true;
 					break;
 				case SDLK_DOWN:
-					if (rectangle.y != 430)rectangle.y += 5;
-					break;
-				case SDLK_LEFT:
-					if (rectangle.x != 0)rectangle.x -= 5;
+					down= true;
 					break;
 				case SDLK_RIGHT:
-					if (rectangle.x != 590)rectangle.x += 5;
+					right = true;
 					break;
+				case SDLK_LEFT:
+					left = true;
+					break;
+				case SDLK_SPACE:
+					space = true;
 				default:
 					break;
 				}
 			}
-			SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
-			SDL_RenderClear(g_pRenderer);
-			SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
-			SDL_RenderFillRect(g_pRenderer, &rectangle);
-			SDL_RenderPresent(g_pRenderer);
-
 		}
+
+		if (rectangle.y != 0 && up) rectangle.y -= 5;
+		if (rectangle.y != 430 && down)rectangle.y += 5;
+		if (rectangle.x != 590 && right)rectangle.x += 5;
+		if (rectangle.x != 0 && left) rectangle.x -= 5;
+		
+		SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
+		SDL_RenderClear(g_pRenderer);
+		SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
+		SDL_RenderFillRect(g_pRenderer, &rectangle);
+		SDL_RenderPresent(g_pRenderer);
 	}
-	SDL_Delay(5000);
+	
 	SDL_Quit();
-		return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
